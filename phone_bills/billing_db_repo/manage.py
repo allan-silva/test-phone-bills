3b10b2155d5264d25f77216777beec1e6061ae1c
allan.silva <allan.tavares@allantavares.com.br>
2018-09-02 21:24:57 -0300

    Database modeling (#2)

---
+++ phone_bills/billing_db_repo/manage.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from migrate.versioning.shell import main
+
+if __name__ == '__main__':
+    main(repository='billing_db_repo', url='postgresql://billingdb_user:billingdb_pwd@0.0.0.0:5432/billingdb', debug='False')
--- phone_bills/billing_db_repo/versions/001_setup.py
+++ phone_bills/billing_db_repo/versions/001_setup.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table, BigInteger, Column, Integer, String, DateTime, DECIMAL, ForeignKey, MetaData
+from sqlalchemy import Table, BigInteger, Column, Integer, String, DateTime, DECIMAL, Time, ForeignKey, MetaData
 
 
 meta = MetaData()
@@ -8,50 +8,53 @@ tariff_conditions = Table(
     'tariff_conditions',
     meta,
     Column('id', Integer, autoincrement=True, primary_key=True),
-    Column('start_date', DateTime, nullable=False),
-    Column('end_date', DateTime, nullable=False))
+    Column('source_area_code', String, nullable=False),
+    Column('destination_area_code', String, nullable=False),
+    Column('start_at', Time, nullable=False),
+    Column('end_at', Time, nullable=False))
 
 
 tariff_configuration = Table(
     'tariff_configuration',
     meta,
     Column('id', Integer, autoincrement=True, primary_key=True),
+    Column('created_date', DateTime, nullable=False),
+    Column('config_start_date', DateTime, nullable=False),
+    Column('config_end_date', DateTime),
     Column('conditions_id',
             Integer,
             ForeignKey(tariff_conditions.c.id),
             nullable=False),
-    Column('created_date', DateTime, nullable=False),
-    Column('start_date', DateTime, nullable=False),
-    Column('end_date', DateTime),
     Column('standard_charge', DECIMAL(8, 3), nullable=False),
-    Column('time_units', Integer, nullable=False),
     Column('call_time_charge', DECIMAL(8, 3), nullable=False))
 
 
-call_entries = Table(
-    'call_entries',
+call_records = Table(
+    'call_records',
     meta,
     Column('id', BigInteger, autoincrement=True, primary_key=True),
     Column('created_date', DateTime, nullable=False),
     Column('external_id', String, nullable=False), # "id": Record unique identificator
     Column('call_id', String, nullable=False),
     Column('type', String, nullable=False),
+    Column('source_area_code', String, nullable=False),
     Column('source', String, nullable=False),
+    Column('destination_area_code', String, nullable=False),
     Column('destination', String, nullable=False),
-    Column('timestamp', DateTime, nullable=False),
-    Column('standard_charge', DECIMAL(8, 3), nullable=False),
-    Column('time_units', Integer, nullable=False),
-    Column('call_time_charge', DECIMAL(8, 3), nullable=False),
-    Column('charge_reference', String),
-    Column('charge_conditions_reference', String))
+    Column('timestamp', DateTime, nullable=False))
 
 
 def upgrade(migrate_engine):
     meta.bind = migrate_engine
     tariff_conditions.create()
     tariff_configuration.create()
-    call_entries.create()
+    call_records.create()
 
 
 def downgrade(migrate_engine):
-    raise NotImplementedError('Setup script')
+    meta.bind = migrate_engine
+    call_records.drop()
+    tariff_configuration.drop()
+    tariff_conditions.drop()
+
+
